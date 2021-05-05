072cbcc6f937df15eb3e97771ea276943d7ce799
Suhas Nidgundi <suhasnidgundi@gmail.com>
2021-05-05 10:24:23 +0530

    Version 3

--- store/views.py
+++ store/views.py
@@ -9,11 +9,31 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+
+    else:
+        # Create Empty Cart for now for none-logged in users
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
 
 def about(request):
